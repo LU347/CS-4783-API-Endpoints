fca6c20154a6ca23bc9a51b5b39d366dca2b15b3
LU347 <103535661+LU347@users.noreply.github.com>
2024-05-13 14:48:44 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -48,32 +48,32 @@ function display_search_results($result)
       $num_results = count( $data );
 
       echo "<table class='view-table' style='border:1px solid; width:90%'>
-                <tr>
-                  <th>MANUFACTURER STATUS</th>
-				  <th>DEVICE STATUS</th>
-                  <th>DEVICE TYPE</th>
-                  <th>MANUFACTURER</th>
-                  <th>SERIAL NUMBER</th>
-                </tr>";
+        <tr>
+           <th>MANUFACTURER STATUS</th>
+	   <th>DEVICE STATUS</th>
+           <th>DEVICE TYPE</th>
+           <th>MANUFACTURER</th>
+           <th>SERIAL NUMBER</th>
+        </tr>";
 
       for ( $i = 0; $i < $num_results; $i++ ) {
         $row = explode( ",", $data[ $i ] );
         echo "<tr>";
         for ( $j = 0; $j < 5; $j++ ) {
-			if (strcmp($row[$j], "ACTIVE") === 0)
-			{
-				echo "<td style='color:green'>";
-				echo $row[ $j ];
-          		echo "</td>";
-			} elseif (strcmp($row[$j], "INACTIVE") === 0) {
-				echo "<td style='color:red'>";
-				echo $row[ $j ];
-          		echo "</td>";
-			} else {
-				echo "<td>";
-				echo $row[ $j ];
-          		echo "</td>";
-			}
+	   if (strcmp($row[$j], "ACTIVE") === 0)
+	   {
+		echo "<td style='color:green'>";
+		echo $row[ $j ];
+          	echo "</td>";
+	   } elseif (strcmp($row[$j], "INACTIVE") === 0) {
+		echo "td style='color:red'>";
+		echo $row[ $j ];
+          	echo "</td>";
+	   } else {
+		echo "<td>";
+		echo $row[ $j ];
+          	echo "</td>";
+	   }
         }
         echo "</tr>";
        }
